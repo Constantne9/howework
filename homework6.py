cae56f7ae2bbaf9ca9d05de9632f9499978f1271
Constantne9 <168212555+Constantne9@users.noreply.github.com>
2024-05-17 21:07:04 +0800

    Add files via upload

--- homework6.py
+++ homework6.py
@@ -3,19 +3,14 @@ students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 students = list(students)
 students.sort()
 a = grades[0]
-a = a[0:]
 a = (sum(a)) / len(grades[0])
 b = grades[1]
-b = b[0:]
 b = (sum(b)) / len(grades[1])
 c = grades[2]
-c = c[0:]
 c = (sum(c)) / len(grades[2])
 d = grades[3]
-d = d[0:]
 d = (sum(d)) / len(grades[3])
 e = grades[4]
-e = e[0:]
 e = (sum(e)) / len(grades[4])
 grades = [a, b, c, d, e]
 GPA = dict()
@@ -24,5 +19,4 @@ GPA.update({students[0]: grades[0],
             students[2]: grades[2],
             students[3]: grades[3],
             students[4]: grades[4]})
-print(GPA)
 print(GPA.items())
